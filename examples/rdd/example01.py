7412b1dfb4ccef410f7dde31ea86d76797f8e4a5
Miroslav Tomic <tomic.miroslav97@gmail.com>
2022-11-15 00:04:41 +0100

    Initial commit

---
+++ examples/rdd/example01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import os
+from pyspark import SparkContext, SparkConf
+
+HDFS_NAMENODE = os.environ["CORE_CONF_fs_defaultFS"]
+
+conf = SparkConf().setAppName("wordcount").setMaster("spark://spark-master:7077")
+sc = SparkContext(conf=conf)
+
+text_file = sc.textFile(HDFS_NAMENODE + "/dante/divine_comedy.txt")
+counts = text_file.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile(HDFS_NAMENODE + "/dante/count")
